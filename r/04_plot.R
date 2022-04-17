9f4bf17881f649fac067c2f19b715d7ec590f229
naohat23 <naokihattori23@gmail.com>
2022-04-18 00:18:01 +0900

    04_plot.Rに軸の設定・ラベルを追加

--- r/04_plot.R
+++ r/04_plot.R
@@ -2,6 +2,10 @@
 
 ## Rで図表を作成する方法
 
+
+
+# パッケージのインポート・設定------------------------------------------------------
+
 library(tidyverse)
 library(geofacet)
 library(ggpubr)
@@ -22,7 +26,7 @@ windowsFonts("YUGO" = windowsFont("Yu Gothic UI"))
 
 
 
-# サンプルデータの確認・作成 -----------------------------------------------------------
+# サンプルデータの確認・読み込み -----------------------------------------------------------
 
 ## Rのサンプルデータセット
 View(mpg)
@@ -40,11 +44,9 @@ data_owid <- readr::read_csv(file = "https://covid.ourworldindata.org/data/owid-
 
 
 
-
-
 # ヒストグラム geom_histogram() --------------------------------------------------
 
-## X軸：連続変数
+## X軸：連続値
 ## Y軸：なし
 
 ## ヒストグラム
@@ -77,7 +79,7 @@ ggplot(data = mpg,
 
 # 密度グラフ geom_density() ----------------------------------------------------
 
-## X軸：連続変数
+## X軸：連続値
 ## Y軸：なし
 
 ## 密度グラフ
@@ -111,7 +113,7 @@ ggplot(data = mpg,
 
 # 頻度棒グラフ geom_bar() ---------------------------------------------------------
 
-## X軸：離散変数
+## X軸：離散値
 ## Y軸：なし
 
 ## 縦棒グラフ
@@ -127,8 +129,8 @@ ggplot(data = mpg,
 
 # 頻度バブルチャート geom_count() -----------------------------------------------------------
 
-## X軸：離散変数
-## Y軸：離散変数
+## X軸：離散値
+## Y軸：離散値
 
 ## バブルチャート（頻度）
 ggplot(data = mpg,
@@ -144,8 +146,14 @@ ggplot(data = mpg,
 
 # 散布図 geom_point() geom_path() geom_smooth() ---------------------------------------------------------------
 
-## X軸：連続変数
-## Y軸：連続変数
+## X軸：連続値
+## Y軸：連続値
+
+## サンプルデータの作成
+data_mpg_point <- mpg %>% 
+  dplyr::group_by(manufacturer) %>% 
+  dplyr::summarise(across(c(displ, cty, hwy), mean, na.rm = TRUE))
+
 
 ## 散布図
 ggplot(data = mpg,
@@ -214,10 +222,8 @@ ggplot(data = mpg,
 
 
 ## 散布図＆ラベル
-mpg %>% 
-  dplyr::group_by(manufacturer) %>% 
-  dplyr::summarise(across(c(cty, hwy), mean, na.rm = TRUE)) %>% 
-  ggplot(mapping = aes(x = cty, y = hwy)) +
+ggplot(data = data_mpg_point,
+       mapping = aes(x = cty, y = hwy)) +
   geom_point(alpha = 1.0, # 塗りつぶしの透明度
              color = "darkblue", # 線の色
              fill = "lightblue", # 塗りつぶしの色
@@ -243,12 +249,9 @@ mpg %>%
                   size = 4.0) # テキストのサイズ
 
 
-
 ## バブルチャート＆ラベル
-mpg %>% 
-  dplyr::group_by(manufacturer) %>% 
-  dplyr::summarise(across(c(displ, cty, hwy), mean, na.rm = TRUE)) %>% 
-  ggplot(mapping = aes(x = cty, y = hwy, size = displ)) + # aes() 関数内のsize引数に連続変数を指定
+ggplot(data = data_mpg_point,
+       mapping = aes(x = cty, y = hwy, size = displ)) + # aes() 関数内のsize引数に連続値を指定
   geom_point(alpha = 1.0, # 塗りつぶしの透明度
              color = "darkblue", # 線の色
              fill = "lightblue", # 塗りつぶしの色
@@ -277,8 +280,8 @@ mpg %>%
 
 # 棒グラフ geom_col() ---------------------------------------------------------
 
-## X軸が離散変数
-## Y軸が連続変数
+## X軸が離散値
+## Y軸が連続値
 
 ## サンプルデータの作成
 data_mpg_col <- mpg %>% 
@@ -399,4 +402,89 @@ data_mpg_col_long %>%
             fontface = "plain", # テキストの書体 (plain / bold / italic / bold.italic)
             size = 4.0) + # テキストのサイズ
   guides(fill = guide_legend(reverse = TRUE))
+
+
+
+# 軸の設定・ラベル ---------------------------------------------------------------------
+
+## サンプルデータの作成
+data_owid_scale <- data_owid %>% 
+  dplyr::group_by(location) %>% 
+  dplyr::summarise(across(c(new_cases, new_deaths), mean, na.rm = TRUE))
+
+summary(data_owid_scale)
+
+
+## X軸：連続値、Y軸：連続値
+ggplot(data = data_owid_scale,
+       mapping = aes(x = new_cases, y = new_deaths)) +
+  geom_point() +
+  geom_smooth() +
+  scale_x_continuous(breaks = breaks_x <- seq(0, 1e+06, 100000), # 目盛
+                     labels = breaks_x, # 目盛ラベル
+                     limits = c(0, 7e+05), # 下限・上限値
+                     expand = expansion(mult = c(0.05, 0.05), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  scale_y_continuous(breaks = breaks_y <- seq(0, 10000, 1000), # 目盛
+                     labels = breaks_y, # 目盛ラベル
+                     limits = c(0, 8000), # 下限・上限値
+                     expand = expansion(mult = c(0.05, 0.05), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  labs(x = "新規感染者数（1日当たり平均、人）", # X軸のラベル
+       y = "新規死亡者数（1日当たり平均、人）") + # Y軸のラベル
+  theme(axis.title = element_text(size = 8)) # 軸ラベルの大きさ
+
+
+## 対数目盛
+ggplot(data = data_owid_scale,
+       mapping = aes(x = new_cases, y = new_deaths)) +
+  geom_point() +
+  geom_smooth() +
+  scale_x_log10(breaks = breaks_x <- 10 ** seq(0, 6, 1), # 目盛
+                     labels = c(breaks_x[1:4], "1万", "10万", "100万"), # 目盛ラベル
+                     limits = c(1, 7e+05), # 下限・上限値
+                     expand = expansion(mult = c(0.05, 0.05), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  scale_y_log10(breaks = breaks_y <- 10 ** seq(0, 4, 1), # 目盛
+                     labels = c(breaks_y[1:4], "1万"), # 目盛ラベル
+                     limits = c(1, 8000), # 下限・上限値
+                     expand = expansion(mult = c(0.05, 0.05), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  labs(x = "新規感染者数（1日当たり平均、人）", # X軸のラベル
+       y = "新規死亡者数（1日当たり平均、人）") + # Y軸のラベル
+  theme(axis.title = element_text(size = 8)) # 軸ラベルの大きさ
+
+
+## X軸：日付、Y軸：連続値（date_breaksを使用する場合）
+ggplot(data = economics,
+       mapping = aes(x = date, y = unemploy)) + 
+  geom_line() +
+  scale_x_date(date_breaks = "3 month",
+               date_labels = "%y/%b", # %Y：4桁年、%y：2桁年、%m：2桁月、%b：1桁月、%d：日
+               limits = c(as.Date("2000/01/01"), as.Date("2002/12/31")),
+               expand = expansion(mult = c(0.00, 0.00), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  scale_y_continuous(breaks = breaks_y <- seq(0, 50000, 1000), # 目盛
+                     labels = breaks_y, # 目盛ラベル
+                     limits = c(5000, 10000), # 下限・上限値
+                     expand = expansion(mult = c(0.05, 0.05), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  labs(x = "日付（年/月）", # X軸のラベル
+       y = "失業者数（千人）") + # Y軸のラベル
+  theme(axis.title = element_text(size = 8)) # 軸ラベルの大きさ
+
+
+## X軸：日付、Y軸：連続値（date_breaksを使用しない場合）
+ggplot(data = economics,
+       mapping = aes(x = date, y = unemploy)) + 
+  geom_line() +
+  scale_x_date(breaks = breaks_x <- seq(as.Date("1967-01-01"), max(economics$date), by = "3 months"),
+               date_labels = str_c(str_sub(lubridate::year(breaks_x), 3, 4), "/", lubridate::month(breaks_x)),
+               limits = c(as.Date("2000/01/01"), as.Date("2002/12/31")),
+               expand = expansion(mult = c(0.00, 0.00), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  scale_y_continuous(breaks = breaks_y <- seq(0, 50000, 1000), # 目盛
+                     labels = breaks_y, # 目盛ラベル
+                     limits = c(5000, 10000), # 下限・上限値
+                     expand = expansion(mult = c(0.05, 0.05), add = c(0, 0))) + # 下限・上限値からの余白（multは余白率、addは余白幅）
+  labs(x = "日付（年/月）", # X軸のラベル
+       y = "失業者数（千人）") + # Y軸のラベル
+  theme(axis.title = element_text(size = 8)) # 軸ラベルの大きさ
+
+  
+
+
 
